d19f75a06bc97387bbadc22c6bcccdbd5c3bf89e
Jhonproyec <albertcasti0919@gmail.com>
2021-11-01 22:41:07 -0600

    correccion para mostrar datos en pantalla, agregé el service de médicos al archivo .ts y los mostré en el select

--- src/app/consultas/consultas.component.ts
+++ src/app/consultas/consultas.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ConsultasService } from '../services/consultas/consultas.service';
+import { MedicosService } from '../services/medicos/medicos.service';
 
 @Component({
   selector: 'app-consultas',
@@ -10,18 +11,31 @@ import { ConsultasService } from '../services/consultas/consultas.service';
 export class ConsultasComponent implements OnInit {
   formularioConsultas: any;
   consultas: any;
+  //En esta variable voy a guardar los datos de los medicos, para mostrarlas en los options del html
+  medicos: any;
 
   constructor(
     public fb: FormBuilder,
     public consultasService: ConsultasService,
+    //Aquí se está llamando al servicio de medicos 
+    public medicosService: MedicosService,
   ) { }
 
   ngOnInit(): void {
-
-
+    //Mostrar consultas
     this.consultasService.traerConsultas()?.subscribe(async consultas => {
-      this.consultas = await consultas;
-  });
+      this.consultas = await consultas;});
+    
+    //Aquí estoy obteniendo los datos de los médicos
+    this.medicosService.traerMedicos().subscribe(medicos => {
+      console.log(medicos);
+      this.medicos = medicos;
+    });
+    
+    
+    
+
+
 }
 
 }
