3d32806ebdde336635948a8996f98a2da0af4cc5
unknown <jc2k03@hotmail.com>
2021-11-07 15:09:36 -0600

    Terminé el CRUD dee mi componente pacientes y la conexión al backend

--- src/app/pacientes/pacientes.component.ts
+++ src/app/pacientes/pacientes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PacientesService } from '../services/pacientes/pacientes.service';
 
 @Component({
   selector: 'app-pacientes',
@@ -7,9 +9,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PacientesComponent implements OnInit {
 
-  constructor() { }
+  pacienteForm: FormGroup;
+  pacientes: any;
+
+  constructor(
+    public fbp: FormBuilder,
+    public pacientesService: PacientesService
+    ) {
+
+  }
 
   ngOnInit(): void {
+    this.pacienteForm = this.fbp.group({
+      idPaciente: [''],
+      nombres: ['', Validators.required],
+      apellidos: ['', Validators.required],
+      dni: ['', Validators.required],
+      direccion: ['', Validators.required],
+      telefono: ['', Validators.required],
+      email: ['', Validators.required],
+    })
+
+    this.pacientesService.traerPacientes().subscribe(resp=>{
+      this.pacientes = resp;
+      this.pacientes.push(resp);
+    },
+      error=>{ console.error(error)}
+    );
+
+  }
+
+  guardar(): void{
+    console.log(this.pacienteForm.value)
+    this.pacientesService.registrar(this.pacienteForm.value).subscribe(resp=>{
+      this.pacienteForm.reset();
+    },
+    error=>{ console.error(error)}
+    )
+  }
+
+  eliminar(paciente:any){
+    this.pacientesService.borrarPaciente(paciente.idPaciente).subscribe(resp=>{
+      this.pacientes.pop(paciente);
+      },
+      error=>{ console.error(error)}
+    )
+  }
+
+  editar(paciente:any){
+    this.pacienteForm.setValue({
+      idPaciente: paciente.idPaciente,
+      nombres: paciente.nombres,
+      apellidos: paciente.apellidos,
+      dni: paciente.dni,
+      direccion:  paciente.direccion,
+      telefono: paciente.telefono,
+      email:  paciente.email,
+    })
   }
 
 }
