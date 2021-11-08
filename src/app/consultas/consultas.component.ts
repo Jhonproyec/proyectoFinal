d0c9eee164450302513fdb8a87878da99e5473f7
RebeckaXL <luna14xirumjr@gmail.com>
2021-11-08 16:40:37 -0600

    se realizo parte para traer datos de especialidades y pacientes

--- src/app/consultas/consultas.component.ts
+++ src/app/consultas/consultas.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ConsultasService } from '../services/consultas/consultas.service';
+import { EspecialidadesService } from '../services/especialidades.service';
 import { MedicosService } from '../services/medicos/medicos.service';
+import { PacientesService } from '../services/pacientes/pacientes.service';
 
 @Component({
   selector: 'app-consultas',
@@ -13,28 +15,42 @@ export class ConsultasComponent implements OnInit {
   consultas: any;
   //En esta variable voy a guardar los datos de los medicos, para mostrarlas en los options del html
   medicos: any;
+  especialidadesService: any;
+  especialidades: any;
+  PacientesService: any;
+  pacientes: any;
 
   constructor(
     public fb: FormBuilder,
     public consultasService: ConsultasService,
-    //Aquí se está llamando al servicio de medicos 
+    //Aquí se está llamando al servicio de medicos
     public medicosService: MedicosService,
+    public EspecialidadesService: EspecialidadesService,
+    public PacienteService: PacientesService,
   ) { }
 
   ngOnInit(): void {
     //Mostrar consultas
     this.consultasService.traerConsultas()?.subscribe(async consultas => {
       this.consultas = await consultas;});
-    
+
     //Aquí estoy obteniendo los datos de los médicos
     this.medicosService.traerMedicos().subscribe(medicos => {
       console.log(medicos);
       this.medicos = medicos;
     });
-    
-    
-    
 
+    //datos especialidades
+    this.especialidadesService.traerEspecialidad().subscribe((especialidades: any) => {
+      console.log(especialidades);
+      this.especialidades = especialidades;
+
+    });
+    //datos pacientes
+    this.PacientesService.traerPaciente().subscribe((pacientes: any) => {
+      console.log(pacientes);
+      this.pacientes = pacientes;
+    });
 
 }
 
